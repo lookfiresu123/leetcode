3cd35cd14855f66eb81d6fb22cbdfb63a2c71418
lookfiresu123 <lookfiresu123@gmail.com>
2016-01-13 20:27:31 +0800

    finish remove_duplicates_from_sorted_list.c

--- remove_duplicates_from_sorted_list.c
+++ remove_duplicates_from_sorted_list.c
@@ -18,68 +18,64 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>
+#include <time.h>
 
 /* Definition for singly-linked list. */
 struct ListNode {
     int val;
     struct ListNode *next;
 };
- 
-#define N 10000
-static int store_nums[N];
-static int count = 0;
 
-bool IsExisted (int value) {
-    int i;
-    for (i = 0 ; i < count ; i++) {
-        if (value == store_nums[i])
-            return true;
+struct ListNode *deleteDuplicates(struct ListNode *head) {
+    struct ListNode *result = NULL, *current_result = result;
+    struct ListNode *current, *previous;
+    for (previous = NULL, current = head ; current ; current = current->next) {
+        if (!previous || previous->val != current->val) {
+            // copy to result
+            if (previous == NULL) {
+                current_result = (struct ListNode *)malloc(1 * sizeof(struct ListNode));
+                result = current_result;
+                current_result->val = current->val;
+                current_result->next = NULL;
+            } else {
+                current_result->next = (struct ListNode *)malloc(1 * sizeof(struct ListNode));
+                current_result = current_result->next;
+                current_result->val = current->val;
+                current_result->next = NULL;
+            }
+        }
+        previous = current;
     }
-    store_nums[count++] = value;
-    return false;
-}
-
-void Del_Node (struct ListNode **current) {
-    struct ListNode *temp = (*current)->next;
-    free(*current);
-    *current = temp;
-}
-
-struct ListNode* deleteDuplicates(struct ListNode* head) {
-    struct ListNode **pos_prev = &head;
-    while (*pos_prev) {
-        if ( IsExisted((*pos_prev)->val) )
-            Del_Node(pos_prev);
-        if (*pos_prev) 			// when the *pos_prev is not the pointer of the tail node
-        	pos_prev = &(*pos_prev)->next;
-    }
-    return head;
-}
-
-void Insert_List (struct ListNode **head_ptr, int value) {
-	while (*head_ptr)
-		head_ptr = &(*head_ptr)->next;
-	*head_ptr = malloc(sizeof(**head_ptr));
-	(*head_ptr)->val = value;
-	(*head_ptr)->next = NULL;
+    return result;
 }
 
 int main (void) {
-	struct ListNode *head = NULL;
-	Insert_List(&head, 1);
-	Insert_List(&head, 2);
-	struct ListNode *current = head;
-	while (current) {
-		printf("%d ", current->val);
-		current = current->next;
-	}
-	printf("\n");
-	head = deleteDuplicates(head);
-	current = head;
-	while (current) {
-		printf("%d ", current->val);
-		current = current->next;
-	}
-	printf("\n");
-	return 0;
+    size_t n = 100;
+    struct ListNode *head = NULL, *new_node = NULL, *previous = NULL;
+    srand(time(NULL));
+    int i;
+    for (i = 0 ; i < n ; i++) {
+        new_node = (struct ListNode *)malloc(sizeof(struct ListNode));
+        if (i == 0) {
+            head = new_node;
+            new_node->val = rand() % 2;
+        } else {
+            previous->next = new_node;
+            new_node->val = previous->val + rand() % 2;
+        }
+        // new_node->val = rand() % 100;
+        new_node->next = NULL;
+        previous = new_node;
+    }
+    struct ListNode *current = NULL;
+    for (current = head ; current ; current = current->next)
+        printf("%d, ", current->val);
+    printf("\n--------------------------------------\n");
+    struct ListNode *result = deleteDuplicates(head);
+    struct ListNode *current_result = NULL;
+    for (current_result = result ; current_result ; current_result = current_result->next)
+        printf("%d, ", current_result->val);
+    free(head);
+    free(result);
+    return 0;
 }
