76370350bf0df39ea5088162c61c5d71b714723f
lookfiresu123 <lookfiresu123@gmail.com>
2016-05-25 19:51:50 +0800

    modify works applications

--- max_xor.cpp
+++ max_xor.cpp
@@ -1,22 +1,25 @@
 #include <vector>
 #include <iostream>
+#include <string>
 
 using namespace std;
 
+// prefix = vec[0...i], suffix = vec[j...n-1]
+// i = -1 ~ n-1 , j = n ~ 0 => i = 0 ~ n, j = n ~ 0
 class Solution {
 public:
     int maxXor(vector<int> &vec) {
         int n = vec.size();
-        // prefix = vec[0...i], suffix = vec[j...n-1]
-        // i = -1 ~ n-1 , j = n ~ 0 => i = 0 ~ n, j = n ~ 0
-        vector<vector<int> > max_panel(n+1, vector<int>(n+1, 0));
+        // use heap instead of stack, because the max limit of stack is 8M and the test data maybe 256 MB
+        vector<vector<int> > *max_panel_ptr = new vector<vector<int> >(n+1, vector<int>(n+1, 0));
         int max = 0;
         for (int i = 1 ; i < n+1 ; ++i) {
             for (int j = n-1 ; j >= 0 ; --j) {
-                max_panel[i][j] = getMax(max_panel[i-1][j] ^ vec[i-1], max_panel[i][j+1] ^ vec[j]);
-                max = getMax(max, max_panel[i][j]);
+                (*max_panel_ptr)[i][j] = getMax((*max_panel_ptr)[i-1][j] ^ vec[i-1], (*max_panel_ptr)[i][j+1] ^ vec[j]);
+                max = getMax(max, (*max_panel_ptr)[i][j]);
             }
         }
+        delete max_panel_ptr;
         return max;
     }
 private:
@@ -26,9 +29,14 @@ private:
 };
 
 int main() {
+    int sz, tmp;
+    cin >> sz;
     vector<int> vec;
-    for (int i = 0 ; i < 3 ; ++i)
-        vec.push_back(i);
+    vec.reserve(sz);
+    for (int i = 0 ; i < sz; ++i) {
+        cin >> tmp;
+        vec.push_back(tmp);
+    }
     Solution sol;
     cout << sol.maxXor(vec) << endl;
     return 0;
--- reg_char.cpp
+++ reg_char.cpp
@@ -1,5 +1,6 @@
 #include <vector>
 #include <iostream>
+#include <string>
 
 using namespace std;
 
@@ -18,13 +19,14 @@ public:
         return ret;
     }
 private:
-    // vec1.size() == vec2.size()
+    // implement the xor operator, and vec1.size() == vec2.size()
     void implXor(vector<int> &vec1, vector<int> &vec2) {
         int n = vec1.size();
         for (int i = 0 ; i < n ; ++i)
             vec1[i] ^= vec2[i];
     }
 
+    // count the number of 1 in vec
     int countForOne(vector<int> &vec) {
         int cnt = 0;
         for (int i = 0 ; i < vec.size() ; ++i)
@@ -33,21 +35,38 @@ private:
         return cnt;
     }
 
+    // this function is used to get the minimun pixel
+    // @n: the number of 1 in vec which calculated by countForOne()
+    // @K: the input data K
     int getMinPixel(int n, int K) {
-        for (int i = 1 ; i <= n ; ++i) {
+        for (int i = 0 ; i <= n ; ++i) {
+            // tmp = 2 ** i
             int tmp = 1;
-            // calculate for C(n, K)
-            for (int j = n ; j > n-i ; --j)
-                tmp *= j;
-            for (int j = K ; j >= 1 ; --j)
-                tmp /= j;
+            for (int j = 0 ; j < i ; ++j)
+                tmp *= 2;
             if (tmp >= K)
-                return tmp;
+                return i;
         }
     }
 };
 
 int main() {
+    // input data
+    int N, M, K;
+    cin >> N >> M >> K;
+    // use pointer here, because the limit size of stack is 8M and the maximum data is 256 MB
+    vector<vector<int> > *vecs_ptr = new vector<vector<int> >(K, vector<int>(N * M, 0));
+    string word, data;
+    while (getline(cin, word))
+        if (! word.empty())
+            data.append(word);
+    // process data
+    int sz = N * M;
+    for (int i = 0 ; i < data.size() ; ++i)
+        (*vecs_ptr)[i / sz][i % sz] = data[i] - '0';
+    // implment data
     Solution sol;
+    cout << sol.regChar(*vecs_ptr) << endl;
+    delete vecs_ptr;
     return 0;
 }
