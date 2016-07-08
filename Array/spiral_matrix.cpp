9ffa3dcbba76498598297612759f082de0547702
lookfiresu123 <lookfiresu123@gmail.com>
2016-07-09 04:38:17 +0800

    modify spiral_matrix_2.cpp and add spiral_matrix.cpp

--- Array/spiral_matrix.cpp
+++ Array/spiral_matrix.cpp
@@ -1,58 +1,61 @@
-#include <vector>
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 class Solution {
 public:
-    vector<vector<int>> generateMatrix(int n) {
-        if (n == 0) {
+    vector<vector<int>> generateMatrix(int m, int n) {
+        if (m == 0 || n == 0) {
             vector<vector<int>> ret;
             return ret;
         }
-        vector<vector<int>> ret(n, vector<int>(n, 0));
-        
+        vector<vector<int>> ret(m, vector<int>(n, 0));
+
         // 假设先从vec[0][-1]的地方跳跃，起初横向跳跃col_step = n次，
-        // 纵向跳跃row_step = n-1次。每次跳跃后都要递减col_step或row_step，且
+        // 纵向跳跃row_step = m-1次。每次跳跃后都要递减col_step或row_step，且
         // 每次跳跃前都要检查col_step和row_step是否大于0，若大于0，则继续执行，
         // 否则退出循环并返回。
-        int row_step = n-1, col_step = n;
+        int row_step = m-1, col_step = n;
         int i = 0, j = -1, k = 0;
         while (true) {
-            
+
             if (col_step <= 0)  break;
             for (int z = 0 ; z < col_step ; ++z)
                 ret[i][++j] = ++k;
             --col_step;
-            
+
             if (row_step <= 0)  break;
             for (int z = 0 ; z < row_step ; ++z)
                 ret[++i][j] = ++k;
             --row_step;
-            
+
             if (col_step <= 0)  break;
             for (int z = 0 ; z < col_step ; ++z)
                 ret[i][--j] = ++k;
             --col_step;
-            
+
             if (row_step <= 0)  break;
             for (int z = 0 ; z < row_step ; ++z)
                 ret[--i][j] = ++k;
             --row_step;
-            
+
         }
         return ret;
     }
 };
 
-int main() {
-    Solution sol;
-    int n = 0;
-    vector<vector<int>> ret = sol.generateMatrix(n);
-    for (vector<vector<int>>::size_type i = 0 ; i < ret.size() ; ++i) {
-        for (vector<int>::size_type j = 0 ; j < ret.size() ; ++j)
+void print(vector<vector<int>> &ret) {
+    for (int i = 0 ; i < ret.size() ; ++i) {
+        for (int j = 0 ; j < ret[i].size() ; ++j)
             cout << ret[i][j] << " ";
         cout << endl;
     }
+}
+
+int main() {
+    Solution sol;
+    vector<vector<int>> ret = sol.generateMatrix(3, 6);
+    print(ret);
     return 0;
 }
