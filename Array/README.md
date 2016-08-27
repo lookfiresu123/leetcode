9e0ebebeb9e3b05aecdd819e91c6b8b0ca2074f5
lookfiresu123 <lookfiresu123@gmail.com>
2016-08-27 19:26:44 +0800

    modify Array/README.md

--- Array/README.md
+++ Array/README.md
@@ -11,10 +11,10 @@ contains_duplicate.cpp | 判断一个数组中是否有重复的数
 contains_duplicate_II.cpp | 判断一个数组中是否有距离不超过K的重复的数
 find_minimun_rotated_sorted_array.cpp | 在一个旋转有序不重复的数组中查找最小值
 find_minimum_in_rotated_sorted_array_2.cpp | 在一个旋转有序有重复的数组中查找最小值
-find_peak_element.cpp |
-game_of_life.cpp |
-majority_element.cpp |
-majority_element_2.cpp |
+find_peak_element.cpp | 在一个无序数组中找到极大值点的索引
+game_of_life.cpp | 细胞状态问题
+majority_element.cpp | 在一个数组中找到出现次数大于n/2次的元素
+majority_element_2.cpp | 在一个数组中找到所有出现次数大于n/2次的元素
 max_of_sliding_window.cpp |
 maximum_subarray.c |
 merge_sorted_array.c |
--- Array/find_peak_element.cpp
+++ Array/find_peak_element.cpp
@@ -5,18 +5,20 @@ using namespace std;
 
 class Exception {};
 
+// 获得极大值点的索引
 class Solution {
 public:
     int findPeakElement_helper(vector<int> &nums, vector<int>::size_type lo, vector<int>::size_type hi) {
         auto mid = lo + (hi - lo) / 2;
         if ((mid == lo && mid == hi) || (mid == lo && nums[mid] > nums[mid+1]) || (mid == hi && nums[mid] > nums[mid-1]))
             return mid;
-        else if (mid < hi && nums[mid] < nums[mid+1])
+        else if (mid < hi && nums[mid] < nums[mid+1])   // 极大值点出现在mid的右边
             findPeakElement_helper(nums, mid+1, hi);
-        else if (mid > lo && nums[mid] < nums[mid-1])
+        else if (mid > lo && nums[mid] < nums[mid-1])   // 极大值点出现在mid的左边
             findPeakElement_helper(nums, lo, mid-1);
         else
             return mid;
+        return 0;
     }
 
     int findPeakElement(vector<int>& nums) {
